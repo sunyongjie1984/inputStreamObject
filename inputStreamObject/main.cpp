de4016085725af018a38b3693240dbd2cbfbd728
yongjies <yongjies@cadence.com>
2022-05-04 22:06:08 +0800

    Initial Commit

---
+++ inputStreamObject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  inputStreamObject
+//
+//  Created by 孙永杰 on 2022/5/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
